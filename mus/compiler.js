c6badf8aa6ee1d846f2a2cc64c571de008ee2f2a
Dafydd Francis <DaveOrdref@gmail.com>
2012-04-15 01:26:58 +0100

    Initial commit

---
+++ mus/compiler.js
@@ -0,0 +1,32 @@
+var compile = function (musexpr) {
+    var note_arr = [];
+    var traverse = function (expr, time) {
+        if(expr.tag === 'par'){
+            return Math.max(traverse(expr.left, time), traverse(expr.right, time)) + time;
+        }else if(expr.tag === 'seq'){
+            time = traverse(expr.left, time);
+            time = traverse(expr.right, time);
+            return time;
+        } else {
+            expr.start = time;
+            note_arr.push(expr);
+            return expr.dur + time;
+        }
+    };
+    traverse(musexpr, 0);
+    return note_arr;
+};
+
+var melody_mus = 
+    { tag: 'seq',
+      left: 
+       { tag: 'seq',
+         left: { tag: 'note', pitch: 'a4', dur: 250 },
+         right: { tag: 'note', pitch: 'b4', dur: 250 } },
+      right:
+       { tag: 'seq',
+         left: { tag: 'note', pitch: 'c4', dur: 500 },
+         right: { tag: 'note', pitch: 'd4', dur: 500 } } };
+
+console.log(melody_mus);
+console.log(compile(melody_mus));
